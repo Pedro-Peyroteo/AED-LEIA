f5518755e7a046594290014c9a7454532bd8e37d
Pedro-Peyroteo <pedropeyroteo@ua.pt>
2025-09-29 15:50:17 +0100

    Chore: Implmented debug message and simple test case.

--- 0 - Revisão/test.c
+++ 0 - Revisão/test.c
@@ -7,7 +7,17 @@ int main(void) {
 
     printf("Count: %zu\nCapacity: %zu\n", nums.count, nums.capacity);
 
+    intlist_append(&nums, 1);
+    intlist_append(&nums, 2);
+    intlist_append(&nums, 3);
+    intlist_append(&nums, 4);
+    intlist_append(&nums, 5);
+
+    printf("Count: %zu\nCapacity: %zu\n", nums.count, nums.capacity);
+
     intlist_free(&nums); // Free any memory used by the list.
+
+    printf("Count: %zu\nCapacity: %zu\nItems: %zu", nums.count, nums.capacity, nums.items);
 
     return 0;
 }
